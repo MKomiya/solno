c9603905363ba5b4ce52aaac48c357df3a411fe1
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2015-03-30 16:55:36 +0900

    tmp: replace state field to story

--- Classes/Model/FieldObject/StoryPoint.cpp
+++ Classes/Model/FieldObject/StoryPoint.cpp
@@ -23,11 +23,11 @@ StoryPoint::StoryPoint(FieldState* state, int id, cocos2d::ValueMap data) :
 void StoryPoint::executeDecideAction()
 {
     auto view = state->getFieldView();
-    
+
     auto fade = FadeOut::create(0.3f);
     auto change_state = CallFunc::create([=]() {
-        auto router = Raciela::Router::getInstance();
-        router->pushState(StoryState::createByStoryId(story_id));
+        // @todo: playerの座標情報を保存しておく
+        Raciela::Router::getInstance()->replaceState(StoryState::createByStoryId(story_id));
     });
     auto action = Sequence::create(fade, change_state, nullptr);
     view->runAction(action);
